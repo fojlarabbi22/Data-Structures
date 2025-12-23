8c4f6625748984ff3e2f2be56e0381b47f14aa70
Fojla Rabbi <fojlarabbikuet22@gmail.com>
2025-12-23 17:09:31 +0600

    Update Basic.cpp

--- Heap/Basic.cpp
+++ Heap/Basic.cpp
@@ -8,19 +8,21 @@ vector<int> heap;
 // Heapify is only possible when both the left and right sub-tree are
 // perfectly maintaining the "heap property"
 void heapify(int i, int n) {
-    int largest = i;
-    int left = 2 * i + 1;
-    int right = 2 * i + 2;
-    if(left < n && heap[left] > heap[largest]) {
-        largest = left;
-    }
-    if(right < n && heap[right] > heap[largest]) {
-        largest = right;
-    }
-    if(largest != i) {
-        swap(heap[i], heap[largest]);
-        i = largest;
-        heapify(i, n);
+    while(true) {
+        int largest = i;
+        int left = 2 * i + 1;
+        int right = 2 * i + 2;
+        if(left < n && heap[left] > heap[largest]) {
+            largest = left;
+        }
+        if(right < n && heap[right] > heap[largest]) {
+            largest = right;
+        }
+        if(largest != i) {
+            swap(heap[i], heap[largest]);
+            i = largest;
+        }
+        else break;
     }
 
 }
@@ -36,14 +38,14 @@ void insert(int val) {
         }
         else break;
     }
-
 }
 
 void delete_root() {
     int n = heap.size();
     heap[0] = heap[n - 1];
     heap.pop_back();
-    heapify(0, n - 1);
+    n--;
+    heapify(0, n);
 
 }
 
@@ -55,15 +57,12 @@ void print_heap() {
 }
 
 int main() {
-
-    //for(int i = 0; i < n; i++) cin >> heap[i];
     insert(100);
     insert(50);
     insert(20);
     insert(200);
     print_heap();
     delete_root();
-    cout << '\n';
     print_heap();
    
     return 0;
